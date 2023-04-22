0cbe6cbc4540d7b9479d2507a262fa71e97b2d50
Lucas Villarreal <lucasvillarreal42@gmail.com>
2023-04-22 11:02:17 -0300

    Creando servidor con Express

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo!')
+const express = require("express") // Importamos ExpressJS
+
+const app = express() // Creamos una app con express
+
+// Le decimos a express que cuando en la url que definimos reciba una peticion responda con determinada respuesta
+app.get("/", (req, res) => {
+    res.send("Hola")
+})
+
+// Definimos en que puerto va a estar escuchando esas peticiones
+app.listen(8080, () => {
+    console.log('Servidor escuchando..')
+})
